195f71758e4041df90f66a014e7c5725a7b2b963
Tristan Leo <tleo@sipgate.de>
2018-02-23 17:17:00 +0100

    extract functions

--- handler/ticket-create.go
+++ handler/ticket-create.go
@@ -20,29 +20,38 @@ func TicketCreateHandler() func(c *gin.Context) {
 			log.Fatal(err)
 			c.AbortWithError(http.StatusInternalServerError, err)
 		} else {
-			firstTicket := ticket.Ticket[0]
-			originalTicketUrl := "***Original ticket***: http://tickets.sipgate.net/otrs/index.pl?Action=AgentTicketZoom;TicketID="+firstTicket.TicketID
-			markdownBody := html2md.Convert(firstTicket.Article[0].Body)
-			markdownBody = originalTicketUrl + "\n\n---\n\n" + markdownBody
+			if len(ticket.Ticket) > 0 {
+				markdownBody, listId, cardTitle := getTicketData(ticket)
+				err := createTrelloCard(cardTitle, markdownBody, listId)
 
-			listId := viper.GetString("trello.ticketCreateListId")
-			cardTitle := fmt.Sprintf("[#%s] %s", firstTicket.TicketID, firstTicket.Title)
-
-			card := trello.Card{
-				Name:   cardTitle,
-				Desc:   markdownBody,
-				IDList: listId,
-			}
-
-			client := trelloClient.NewClient()
-			err := client.CreateCard(&card, trello.Defaults())
-
-			if err == nil {
-				c.AbortWithStatus(http.StatusAccepted)
+				if err == nil {
+					c.AbortWithStatus(http.StatusAccepted)
+				} else {
+					log.Println(err)
+					c.AbortWithError(500, err)
+				}
 			} else {
-				log.Println(err)
-				c.AbortWithError(500, err)
+				c.AbortWithStatusJSON(404, gin.H{"message": "Ticket with ID " + ticketId + " not found"})
 			}
 		}
 	}
+}
+func createTrelloCard(cardTitle string, markdownBody string, listId string) error {
+	card := trello.Card{
+		Name:   cardTitle,
+		Desc:   markdownBody,
+		IDList: listId,
+	}
+	client := trelloClient.NewClient()
+	err := client.CreateCard(&card, trello.Defaults())
+	return err
+}
+func getTicketData(ticket otrs.TicketResponse) (string, string, string) {
+	firstTicket := ticket.Ticket[0]
+	originalTicketUrl := "***Original ticket***: http://tickets.sipgate.net/otrs/index.pl?Action=AgentTicketZoom;TicketID=" + firstTicket.TicketID
+	markdownBody := html2md.Convert(firstTicket.Article[0].Body)
+	markdownBody = originalTicketUrl + "\n\n---\n\n" + markdownBody
+	listId := viper.GetString("trello.ticketCreateListId")
+	cardTitle := fmt.Sprintf("[#%s] %s", firstTicket.TicketID, firstTicket.Title)
+	return markdownBody, listId, cardTitle
 }
