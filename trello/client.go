0b6661fcd3ec76b910f6ecb8439ddb404cd9d393
Tristan Leo <tleo@sipgate.de>
2018-02-28 18:17:20 +0100

    make proxy usage configurable

--- trello/client.go
+++ trello/client.go
@@ -5,11 +5,20 @@ import (
 	"github.com/spf13/viper"
 	"net/url"
 	"net/http"
+	"log"
+	"github.com/pkg/errors"
 )
 
 func NewClient() *trello.Client {
 	client := trello.NewClient(viper.GetString("trello.appKey"), viper.GetString("trello.token"))
-	proxyUrl, _ := url.Parse("http://proxy.sipgate.net:8888")
-	client.Client = &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	proxy := viper.GetString("trello.proxy")
+	if proxy != "" {
+		proxyUrl, err := url.Parse(proxy)
+		if err != nil {
+			log.Panicln(errors.Wrapf(err, "could not parse proxy url %s", proxy))
+		}
+		client.Client = &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	}
+
 	return client
 }
